1a8ffe7ea0f3ed5cdedfcb68059c7cd10d1b722a
Bushra Ibrahim <106638675+bushrimp@users.noreply.github.com>
2022-11-11 11:04:53 -0600

    Update GoC_Objects.py

--- GoC_Objects.py
+++ GoC_Objects.py
@@ -4,9 +4,16 @@
 Created on Tue Oct 25 09:28:04 2022
 
 @author: bushra
-"""
 
-# 1 - graph with 7 vertices and 3 cycles
+This is a non-exhaustive library of graphs we have entered
+in the GoC form, to be reused so that we don't have to retype
+certain graphs that we already have typed.
+
+For graphs with cycles - a Y means it worked with the DFS
+                         a N means it did not
+"""
+# 1 - graph with 7 vertices and 3 cycles - Y
+#     (a triangle, and 2 quadrilaterals attached)
 
 test = GameofCycles(7)
 test.addEdge(5,4)
@@ -20,7 +27,7 @@ test.addEdge(1,6)
 test.addEdge(3,0)
 test.addEdge(6,2)
 
-# 2 - tree with 11 vertices
+# 2 - tree with 11 vertices 
 
 test = GameofCycles(11)
 test.addEdge(1,0)
@@ -34,7 +41,8 @@ test.addEdge(8,6)
 test.addEdge(9,6)
 test.addEdge(10,6)
 
-# 3 - graph with 8 verticies and 2 cycles
+# 3 - graph with 8 verticies and 2 cycles - Y
+#     (stick with triangle, stick with square)
 
 test = GameofCycles(8)
 test.addEdge(4,1)
@@ -47,7 +55,8 @@ test.addEdge(7,2)
 test.addEdge(6,4)
 test.addEdge(3,7)
 
-# 4 - graph with 6 vertices and 4 cycles (DFS can't handle this one)
+# 4 - graph with 6 vertices and 4 cycles - N
+#     (4 triangles within triangle)
 
 test = GameofCycles(6)
 test.addEdge(0,1)
@@ -60,7 +69,8 @@ test.addEdge(2,5)
 test.addEdge(3,4)
 test.addEdge(3,5)
 
-# 5 - graph with 9 vertices with 3 cycles
+# 5 - graph with 9 vertices with 3 cycles - Y
+#     (square with diagonal, a quad attached to 1 vertex, and extra branch)
 
 test = GameofCycles(9)
 test.addEdge(0,3)
@@ -75,7 +85,8 @@ test.addEdge(1,6)
 test.addEdge(1,7)
 test.addEdge(8,6)
 
-# 6 - graph with 10 vertices and 7 cycles (DFS struggling)
+# 6 - graph with 10 vertices and 7 cycles - N
+#     (larger stacked triangle, 5 3-cycles, 2 4-cycles)
 
 test = GameofCycles(10)
 test.addEdge(0,1)
@@ -95,7 +106,8 @@ test.addEdge(8,5)
 test.addEdge(7,5)
 test.addEdge(2,6)
 
-# 7 - graph with 10 vertices and 5 cycles
+# 7 - graph with 10 vertices and 5 cycles - N
+#     (3 squares, 2 with diagonals, one without)
 
 test = GameofCycles(10)
 
@@ -114,7 +126,8 @@ test.addEdge(9,7)
 test.addEdge(5,1)
 test.addEdge(0,7)
 
-# 8 - graph with 6 vertices and 1 cycle 
+# 8 - graph with 6 vertices and 1 cycle - Y
+#     (square with extra branch)
 
 test.addEdge(0,1)
 test.addEdge(0,3)
@@ -123,7 +136,8 @@ test.addEdge(3,2)
 test.addEdge(5,4)
 test.addEdge(3,4)
 
-# 9 - graph with 15 vertices and 4 cycles (edited for testing later)
+# 9 - graph with 15 vertices and 4 cycles - Y
+#    (house with chimney and door)
 
 test = GameofCycles(15)
 
@@ -146,7 +160,8 @@ test.addEdge(6,13)
 test.addEdge(13,8)
 test.addEdge(8,14)
 
-# 10 - graph with 5 vertices and 3 cycles (DFS can't handle)
+# 10 - graph with 5 vertices and 3 cycles - N
+#      (rectangle with triangle in it)
 
 test = GameofCycles(5)
 
@@ -158,17 +173,19 @@ test.addEdge(0,2)
 test.addEdge(4,2)
 test.addEdge(2,1)
 
-# 11 - graph with 4 vertices and 2 cycles (DFS can't handle - smallest instance?)
+# 11 - graph with 4 vertices and 2 cycles - N*
+#      (square with diagonal)
 
-test = GameofCycles(5) #it can handle it in this order, but not a different order
-
+test = GameofCycles(5) 
+#* except in this order
 test.addEdge(0,1)
 test.addEdge(0,2)
 test.addEdge(0,3)
 test.addEdge(1,2)
 test.addEdge(3,2)
 
-# 12 - graph with 6 vertices and 3 cycles (DFS handles in this order)
+# 12 - graph with 6 vertices and 3 cycles - Y
+#      (trapezoid with 1 triangle on each side)
 
 test = GameofCycles(6)
 
@@ -181,7 +198,8 @@ test.addEdge(2,3)
 test.addEdge(3,4)
 test.addEdge(4,5)
 
-# 13 - graph with 4 verices and 3 cycles (K4) (DFS can't handle)
+# 13 - graph with 4 verices and 3 cycles - N
+#      (K4, square with diagonal and external swoop)
 
 test = GameofCycles(4)
 
@@ -192,7 +210,8 @@ test.addEdge(1,3)
 test.addEdge(1,2)
 test.addEdge(2,3)
 
-# 14 - graph with 3 vertices and 1 cycle (a triangle)
+# 14 - graph with 3 vertices and 1 cycle - Y
+#      (a triangle)
 
 test = GameofCycles(3)
 
@@ -200,7 +219,8 @@ test.addEdge(0,1)
 test.addEdge(0,2)
 test.addEdge(2,1)
 
-# 15 - graph with 4 vertices and 1 cycle (a square)
+# 15 - graph with 4 vertices and 1 cycle - Y
+#      (a square)
 
 test = GameofCycles(4)
 
@@ -209,7 +229,8 @@ test.addEdge(1,3)
 test.addEdge(3,0)
 test.addEdge(1,2)
 
-# 16 - graph with 5 vertices and 2 cycles (a house)
+# 16 - graph with 5 vertices and 2 cycles - N
+#      (a house)
 
 test = GameofCycles(5)
 
@@ -219,3 +240,20 @@ test.addEdge(3,0)
 test.addEdge(1,4)
 test.addEdge(0,2)
 
+# 17 - graph with 8 vertices and 5 cycles - N
+#      (square with a triangle on each side)
+
+test = GameofCycles(8)
+
+test.addEdge(0,1)
+test.addEdge(1,2)
+test.addEdge(2,3)
+test.addEdge(3,4)
+test.addEdge(4,5)
+test.addEdge(5,6)
+test.addEdge(6,7)
+test.addEdge(7,0)
+test.addEdge(1,3)
+test.addEdge(3,5)
+test.addEdge(5,7)
+test.addEdge(7,1)
